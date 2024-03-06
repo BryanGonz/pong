12679bcaa777040c89d73b3655b0b6a1760632b0
BryanGonz <bgon@ucla.edu>
2024-03-06 08:36:24 -0600

    Got the pong paddles and ball on the screen.

--- firstcppgame/game.cpp
+++ firstcppgame/game.cpp
@@ -2,20 +2,20 @@
 #define pressed(b)  (input->buttons[b].is_down && input->buttons[b].changed)
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
 
-float player_pos_x = 0.f;
-float player_pos_y = 0.f;
-//#3 7:14
+float player_p = 0.f;
+
 internal void
 simulate_game(Input* input, float dt) {
 	clear_screen(0xff5500);
 
-	float speed = 5.f; 
-	if (pressed(BUTTON_UP))	   player_pos_y += speed; 
-	if (pressed(BUTTON_DOWN))  player_pos_y -= speed;
-	if (pressed(BUTTON_RIGHT)) player_pos_x += speed;
-	if (pressed(BUTTON_LEFT))  player_pos_x -= speed;
+	draw_rect(0, 0, 85, 45, 0xffaa33);
 
-	draw_rect(player_pos_x, player_pos_y, 1, 1, 0x00ff22);
-	draw_rect(30, 30, 5, 5, 0x00ff22);
-	draw_rect(-20, 0, 8, 3, 0x00ff2);
+	float speed = 5.f; 
+	if (is_down(BUTTON_UP))	   player_p += speed*dt; 
+	if (is_down(BUTTON_DOWN))  player_p -= speed*dt;
+
+	draw_rect(0, 0, 1, 1, 0xffffff);
+
+	draw_rect(80, player_p, 2.5, 12, 0xff0000);
+	draw_rect(-80, 0, 2.5, 12, 0xff0000);
 }
